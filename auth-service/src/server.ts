3c36cd99492d8589dea0cbda61edc8f7f914e8fc
NithinKS007 <nithinksndkm@gmail.com>
2025-12-02 18:42:55 +0530

    every services refactor completed

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import { Request, Response } from "express";
@@ -13,32 +12,24 @@ import {
   rateLimiter,
 } from "../../utils/src";
 import { IMessageService } from "interfaces/interfaces";
+import { envConfig } from "config/env.config";
 
 dotenv.config();
 
 const app = express();
-const allowedOrigins = process.env.CLIENT_ORIGINS;
 
-// Security & core middleware
 app.use(helmet());
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
-  })
-);
 app.use("/api", rateLimiter);
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 // Health & basic routes
 app.get("/health", (req: Request, res: Response) => {
-  res.json({ status: "ok", service: "auth-service", port: process.env.PORT });
+  res.json({ status: "ok", service: "auth-service", port: envConfig.PORT });
 });
 
 app.get("/", (req: Request, res: Response) => {
-  res.json({ message: `message send from server ${process.env.PORT}` });
+  res.json({ message: `message send from server ${envConfig.PORT}` });
 });
 
 // API routes
@@ -60,6 +51,6 @@ messageService
   .catch((err: Error) => console.error(err));
 
 // Start server
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}.`);
+app.listen(envConfig.PORT, () => {
+  console.log(`Server is running on port ${envConfig.PORT}.`);
 });
--- user-service/src/config/env.config.ts
+++ user-service/src/config/env.config.ts
@@ -3,18 +3,26 @@ import dotenv from "dotenv";
 dotenv.config();
 interface Env {
   PORT: number;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: number;
+  POSTGRES_DB: string;
   SERVICE_NAME: string;
-  DATABASE_URL:string
-  KAFKA_BROKERS:string
-  KAFKA_CLIENT_ID:string
-  KAFKA_GROUP_ID:string
-  KAFKA_ENABLED:string
+  DATABASE_URL: string;
+  KAFKA_BROKERS: string;
+  KAFKA_CLIENT_ID: string;
+  KAFKA_GROUP_ID: string;
+  KAFKA_ENABLED: string;
 }
 
 export const envConfig: Env = {
   PORT: Number(process.env.PORT) || 3000,
   SERVICE_NAME: process.env.SERVICE_NAME || "user-service",
-  DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/postgres",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://postgres:postgres@localhost:5432/postgres",
+  POSTGRES_USER: process.env.POSTGRES_USER || "postgres",
+  POSTGRES_PASSWORD: Number(process.env.POSTGRES_PASSWORD) || 5432,
+  POSTGRES_DB: process.env.POSTGRES_DB || "postgres",
   KAFKA_BROKERS: process.env.KAFKA_BROKERS || "localhost:9092",
   KAFKA_CLIENT_ID: process.env.KAFKA_CLIENT_ID || "user-service",
   KAFKA_GROUP_ID: process.env.KAFKA_GROUP_ID || "user-service-group",
