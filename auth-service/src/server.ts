93eb408131d79a9ee1b3fc8f4c40b790a5c17008
NithinKS007 <nithinksndkm@gmail.com>
2026-02-01 14:44:50 +0530

    corrected bug with starting containers with proto file

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -1,4 +1,4 @@
-import { container } from "container";
+import { container } from "./container";
 import { KafkaService, logger } from "../../utils/src";
 import { app } from "./app";
 import { envConfig } from "./config/env.config";
